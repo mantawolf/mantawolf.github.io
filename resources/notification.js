40a79a77556eb7c22d8b8d1204a45df270d21bbb
Marvin Eads <marvin.eads@eadshome.net>
2015-04-13 20:04:31 -0500

    Script change to FormData obj

--- resources/notification.js
+++ resources/notification.js
@@ -1,12 +1,12 @@
 function postContactForm(){
     var xmlhttp = new XMLHttpRequest();
     var url = "https://getsimpleform.com/messages/ajax?form_api_token=2cf6bd15735e916486a2660d1143d342";
-    var data = {};
+    var data = new FormData();
 
-    data["name"] = document.getElementById("name").value;
-    data["email"] = document.getElementById("email").value;
-    data["title"] = document.getElementById("title").value;
-    data["message"] = document.getElementById("message").value;
+    data.append("name", document.getElementById("name").value);
+    data.append("email", document.getElementById("email").value);
+    data.append("title", document.getElementById("title").value);
+    data.append("message", document.getElementById("message").value);
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
