ae260a410ac9601cb32f6001c543c3c0add39449
Marvin Eads <marvin.eads@eadshome.net>
2015-05-18 19:11:55 -0500

    added post script for signup page

--- resources/notification.js
+++ resources/notification.js
@@ -20,6 +20,30 @@ function postContactForm(){
     console.log(xmlhttp);
 }
 
+function postSignupForm(){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://getsimpleform.com/messages/ajax?form_api_token=2cf6bd15735e916486a2660d1143d342";
+    var data = new FormData();
+
+    url += "&name=" + document.getElementById("name").value;
+    url += "&email=" + document.getElementById("email").value;
+    url += "&subject=" + document.getElementById("birthdate").value;
+    url += "&message=" + document.getElementById("birthdate").value;
+    url += "&message=" + document.getElementById("address").value;
+    url += "&message=" + document.getElementById("phone").value;
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            toggleNotification('success', 'Signup successful.  You should receive information shortly.');
+        }
+    }
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(data);
+
+    console.log(xmlhttp);
+}
+
 function toggleNotification(type, msg) {
     var notificationDiv = document.getElementById("notification");
 	var hideNotification = function(){
